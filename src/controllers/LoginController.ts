865b83f752d72c8e0d2c5dd38132e43ecc7e02c4
Yago Gomes <yagogomesc@gmail.com>
2021-03-07 20:28:30 -0300

    Adiciona rota de login e serviço

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,20 +1,28 @@
 import { Request, Response } from "express";
 import { compare, hash } from "bcrypt";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+import { AppError } from "../errors/AppError";
 
 class LoginController {
   async login(request: Request, response: Response) {
     const { email, password } = request.body;
 
-    const passHashed = userRepository.findOne({ email });
+    const usersRepository = getCustomRepository(UsersRepository);
 
-    const passwordCheck = await compare(password, passHashed.password);
+    const user = await usersRepository.findOne({ email });
 
-    // const user = userRepository.create({
-    //   email,
-    //   password,
-    // });
+    if (!user) {
+      throw new AppError("Username or Password was invalid");
+    }
 
-    return response.status(201).json("user");
+    const passwordCheck = await compare(password, user.password);
+
+    if (!passwordCheck) {
+      throw new AppError("Username or Password was invalid");
+    }
+
+    return response.status(201).json("Login OK");
   }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { LoginController } from "./controllers/LoginController";
 import { RegisterController } from "./controllers/RegisterController";
 
 const registerController = new RegisterController();
+const loginController = new LoginController();
 const router = Router();
 
 router.post("/register", registerController.register);
+
+router.post("/login", loginController.login);
 
 export { router };
