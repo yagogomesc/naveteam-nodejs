f06e6498852ac9f83c2f7f310c3e5d69c5b3a6a0
Yago Gomes <yagogomesc@gmail.com>
2021-03-07 11:42:39 -0300

    Adiciona tsconfig e comando dev

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { router } from "./routes";
 
 const app = express();
