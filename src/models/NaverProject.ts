9ae0b3e1ec26ae064b9dbc8c1ae309597f347e1b
Yago Gomes <yagogomesc@gmail.com>
2021-03-07 21:58:02 -0300

    Ajustado entidade

--- src/models/NaverProject.ts
+++ src/models/NaverProject.ts
@@ -1,17 +1,17 @@
-import { Column, Entity, JoinColumn, ManyToMany } from "typeorm";
+import { Entity, JoinColumn, ManyToMany, PrimaryColumn } from "typeorm";
 import { Naver } from "./Naver";
 import { Project } from "./Project";
 
 @Entity("navers_projects")
 class NaverProject {
-  @Column()
+  @PrimaryColumn()
   naver_id: number;
 
   @ManyToMany(() => Naver)
   @JoinColumn({ name: "naver_id" })
   naver: Naver;
 
-  @Column()
+  @PrimaryColumn()
   project_id: number;
 
   @ManyToMany(() => Project)
