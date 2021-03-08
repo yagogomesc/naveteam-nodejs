4a699a0e3279ef07b08674ecbf3ba9d40665cd41
Yago Gomes <yagogomesc@gmail.com>
2021-03-07 21:58:47 -0300

    Criado funções para naver controller

---
+++ src/controllers/NaverController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+class NaverController {
+  async index(request: Request, response: Response) {}
+
+  async create(request: Request, response: Response) {}
+
+  async show(request: Request, response: Response) {}
+}
+
+export { NaverController };
