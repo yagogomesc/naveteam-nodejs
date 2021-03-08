44932939c44a80568977262ae0a277bb3b609f66
Yago Gomes <yagogomesc@gmail.com>
2021-03-07 21:59:21 -0300

    Add middleware a aplicação para verificar jwt token

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import { LoginController } from "./controllers/LoginController";
+import { ProjectController } from "./controllers/ProjectController";
 import { RegisterController } from "./controllers/RegisterController";
+import { authenticateToken } from "./middlewares/authenticateUserToken";
 
 const registerController = new RegisterController();
 const loginController = new LoginController();
+const projectController = new ProjectController();
 const router = Router();
 
 router.post("/register", registerController.register);
 
 router.post("/login", loginController.login);
+
+router.use(authenticateToken);
+
+router.get("/projects", projectController.test);
 
 export { router };
