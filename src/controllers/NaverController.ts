f72509185e7ab4e5282e65e738c6036400c3b89d
Yago Gomes <yagogomesc@gmail.com>
2021-03-08 22:54:29 -0300

    Criado funções para criar naver e projeto

--- src/controllers/NaverController.ts
+++ src/controllers/NaverController.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { NaversRepository } from "../repositories/NaversRepository";
 
 class NaverController {
   async index(request: Request, response: Response) {}
 
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    const { name, birthdate, admission_date, job_role } = request.body;
+    const naversRepository = getCustomRepository(NaversRepository);
+
+    const naver = naversRepository.create({
+      user_id: request.id,
+      name,
+      birthdate,
+      admission_date,
+      job_role,
+    });
+
+    await naversRepository.save(naver);
+
+    return response.status(200).json(naver);
+  }
 
   async show(request: Request, response: Response) {}
 }
--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { ProjectsRepository } from "../repositories/ProjectsRepository";
 
 class ProjectController {
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    const { name, navers } = request.body;
+    const projectsRepository = getCustomRepository(ProjectsRepository);
+
+    const project = projectsRepository.create({
+      user_id: request.id,
+      name,
+    });
+
+    await projectsRepository.save(project);
+
+    projectsRepository.storeProjectNavers(request.id, project.id, navers);
+
+    return response.status(200).json(project);
+  }
 }
 
 export { ProjectController };
