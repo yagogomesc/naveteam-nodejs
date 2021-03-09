f102b2775c8246b52905456c7c164c35672746f7
Yago Gomes <yagogomesc@gmail.com>
2021-03-08 22:54:51 -0300

    Adicionado rotas para funçoes criar do projeto e naver

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { LoginController } from "./controllers/LoginController";
+import { NaverController } from "./controllers/NaverController";
 import { ProjectController } from "./controllers/ProjectController";
 import { RegisterController } from "./controllers/RegisterController";
 import { authenticateToken } from "./middlewares/authenticateUserToken";
@@ -7,6 +8,7 @@ import { authenticateToken } from "./middlewares/authenticateUserToken";
 const registerController = new RegisterController();
 const loginController = new LoginController();
 const projectController = new ProjectController();
+const naverController = new NaverController();
 const router = Router();
 
 router.post("/register", registerController.register);
@@ -15,6 +17,8 @@ router.post("/login", loginController.login);
 
 router.use(authenticateToken);
 
-router.get("/projects", projectController.create);
+router.post("/project", projectController.create);
+
+router.post("/naver", naverController.create);
 
 export { router };
