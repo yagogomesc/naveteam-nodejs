e46b4829aa9f8b6ad8ef24346ce54409c4cfbd69
Yago Gomes <yagogomesc@gmail.com>
2021-03-08 22:54:12 -0300

    Criado funções dos repositorios

--- src/repositories/NaversProjectsRepository.ts
+++ src/repositories/NaversProjectsRepository.ts
@@ -2,6 +2,36 @@ import { EntityRepository, Repository } from "typeorm";
 import { NaverProject } from "../models/NaverProject";
 
 @EntityRepository(NaverProject)
-class NaversProjectsRepository extends Repository<NaverProject> {}
+class NaversProjectsRepository extends Repository<NaverProject> {
+  async storeMultiNavers({ navers_ids, project_id }): Promise<void> {
+    const naversAndProjects = [];
+
+    navers_ids.forEach((id) => {
+      naversAndProjects.push({
+        naver_id: id,
+        project_id,
+      });
+    });
+
+    const naverProject = this.create(naversAndProjects);
+
+    await this.save(naverProject);
+  }
+
+  async storeMultiProjects(naver_id: number, projects_ids: number[]) {
+    const naversAndProjects = [];
+
+    projects_ids.forEach((id) => {
+      naversAndProjects.push({
+        naver_id,
+        project_id: id,
+      });
+    });
+
+    const naverProject = this.create(naversAndProjects);
+
+    await this.save(naverProject);
+  }
+}
 
 export { NaversProjectsRepository };
--- src/repositories/ProjectsRepository.ts
+++ src/repositories/ProjectsRepository.ts
@@ -1,7 +1,43 @@
-import { EntityRepository, Repository } from "typeorm";
+import { EntityRepository, getCustomRepository, Repository } from "typeorm";
+import { AppError } from "../errors/AppError";
 import { Project } from "../models/Project";
+import { NaversProjectsRepository } from "./NaversProjectsRepository";
+import { NaversRepository } from "./NaversRepository";
+import { UsersRepository } from "./UsersRepository";
 
 @EntityRepository(Project)
-class ProjectsRepository extends Repository<Project> {}
+class ProjectsRepository extends Repository<Project> {
+  public async storeProjectNavers(
+    user_id: number,
+    project_id: number,
+    navers: number[]
+  ) {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const naversRepository = getCustomRepository(NaversRepository);
+    const naversProjectsRepository = getCustomRepository(
+      NaversProjectsRepository
+    );
+    const userExists = usersRepository.find({ id: user_id });
+
+    if (!userExists) {
+      throw new AppError("Invalid user", 404);
+    }
+
+    await Promise.all(
+      navers.map(async (id) => {
+        const naverExists = await naversRepository.find({ id });
+
+        if (!naverExists) {
+          throw new AppError(`Naver with id ${id} not found`, 404);
+        }
+      })
+    );
+
+    await naversProjectsRepository.storeMultiNavers({
+      navers_ids: navers,
+      project_id: project_id,
+    });
+  }
+}
 
 export { ProjectsRepository };
