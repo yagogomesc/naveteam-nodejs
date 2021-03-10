e28723de5df86b10be11a7bfdccb875daba849a1
Yago Gomes <yagogomesc@gmail.com>
2021-03-10 14:46:28 -0300

    Add função de achar projetos com query de busca

--- src/repositories/ProjectsRepository.ts
+++ src/repositories/ProjectsRepository.ts
@@ -1,8 +1,17 @@
-import { EntityRepository, getCustomRepository, Repository } from "typeorm";
+import {
+  EntityRepository,
+  getCustomRepository,
+  getRepository,
+  Repository,
+} from "typeorm";
 import { AppError } from "../errors/AppError";
 import { Project } from "../models/Project";
 import { NaversProjectsRepository } from "./NaversProjectsRepository";
 import { NaversRepository } from "./NaversRepository";
+
+interface projectsQueryInterface {
+  name?: string;
+}
 
 @EntityRepository(Project)
 class ProjectsRepository extends Repository<Project> {
@@ -26,6 +35,21 @@ class ProjectsRepository extends Repository<Project> {
       navers_ids: navers,
       project_id: project_id,
     });
+  }
+
+  public async findAllByQuery(user_id: number, query: projectsQueryInterface) {
+    let whereClause = "user_id = :user_id";
+    const whereVariables = { user_id };
+
+    if (query.name) {
+      whereClause += " AND LOWER(name) LIKE LOWER(:name)";
+      Object.assign(whereVariables, { name: `%${query.name}%` });
+    }
+
+    return getRepository(Project)
+      .createQueryBuilder()
+      .where(whereClause, whereVariables)
+      .getMany();
   }
 }
 
